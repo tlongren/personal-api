79108e1c466b24f7e3a0a93f9c74af7626ba74d1
tlongren <tlongren@gmail.com>
2013-12-15 21:48:34 -0600

    Rename and update.

--- index.php
+++ index.php
@@ -1,36 +1,69 @@
 <?php
-// Pull random data from longren.org API
+header('Content-Type: application/json');
 
-$url = "http://www.longren.org/api.php";
-$result = file_get_contents($url);
-$json = json_decode($result);
-
-// get years
-$years = $json->person->age->years;
+$json = '{
+    "person": {
+        "age": {
+            "years": 30
+        },
+        "location": {
+            "latitude": 41.8819,
+            "longitude": -87.6278,
+            "city": "Nevada",
+            "state": "IA"
+        },
+        "languages": {
+            "communication": ["English"],
+            "programming": ["Javascript", "HTML", "CSS", "PHP"],
+            "frameworks": ["WordPress", "Anchor", "LESS", "SASS", "jQuery", "PostgreSQL", "MariaDB", "MySQL"]
+        },
+        "employer": {
+            "name": "Visionary Services",
+            "services": "Web Development and Design",
+            "position": "Software Engineer",
+            "city": "Des Moines",
+            "state": "IA"
+        },
+        "family": {
+            "wife": "Kayla",
+            "daughter": "Sydney"
+        }
+    },
+    "identifiers": {
+        "image": "https://lh6.googleusercontent.com/-G_Cw2kSHK2w/UcwDnydyQyI/AAAAAAAAa4A/sQe4_fKbHf8/w320-h240-no/smh.gif",
+        "home_url": "http://www.longren.org",
+        "email": "tlongren@gmail.com",
+        "first_name": "Tyler",
+        "last_name": "Longren",
+        "externals": [{
+            "name": "Twitter",
+            "username": "tlongren",
+            "html_url": "http://twitter.com/tlongren"
+        }, {
+            "name": "GitHub",
+            "username": "tlongren",
+            "html_url": "http://github.com/tlongren"
+        }]
+    },
+    "blog": {
+        "rss": "http://www.longren.org/feed/",
+        "featured": [{
+            "title": "Infographic: Global Cell Phone Statistics",
+            "url": "http://www.longren.org/infographic-global-cell-phone-statistics/",
+            "date": "2013-12-15T04:15:28.513Z"
+        }]
+    },
+    "_links": {
+        "self": {
+            "href": "/"
+        },
+        "kegplan": {
+            "href": "http://kegplan.io/"
+        },
+        "vpsstat.us": {
+            "href": "https://vpsstat.us/"
+        }
+    }
+}';
+echo $json;
 ?>
-
-<pre>
-<?=$years?>
-</pre>
-
-<?php
-// get programming languages
-$programming_languages = $json->person->languages->programming;
-$coding = array(); $i=0; $code_langs = array();
-foreach ($programming_languages as $pr) {   
-    $code_langs[] = $pr;
-}
-?>
-
-<pre>
-<?php print_r($code_langs); ?>
-</pre>
-
-<?php
-// Get Family
-$family = $json->person->family;
-?>
-
-<pre>
-<?php print_r($family); ?>
-</pre>
---
+++ php-example.php
@@ -0,0 +1,36 @@
+<?php
+// Pull random data from longren.org API
+
+$url = "http://api.longren.org";
+$result = file_get_contents($url);
+$json = json_decode($result);
+
+// get years
+$years = $json->person->age->years;
+?>
+
+<pre>
+<?=$years?>
+</pre>
+
+<?php
+// get programming languages
+$programming_languages = $json->person->languages->programming;
+$coding = array(); $i=0; $code_langs = array();
+foreach ($programming_languages as $pr) {   
+    $code_langs[] = $pr;
+}
+?>
+
+<pre>
+<?php print_r($code_langs); ?>
+</pre>
+
+<?php
+// Get Family
+$family = $json->person->family;
+?>
+
+<pre>
+<?php print_r($family); ?>
+</pre>
